bdc3325db69660fe551637ff6f81b31d047f3ff4
Becka Catanzaro <becka@Beckas-MacBook-Pro.local>
2019-12-21 16:43:32 -0500

    updated permute_a_palindrome.js with comments

--- permute_a_palindrome.js
+++ permute_a_palindrome.js
@@ -1,3 +1,15 @@
+// https://www.codewars.com/kata/58ae6ae22c3aaafc58000079
+
+// Write a function that will check whether the permutation of an input string is a palindrome. Bonus points for a solution that is efficient and/or that uses only built-in language functions. Deem yourself brilliant if you can come up with a version that does not use any function whatsoever.
+
+// Example
+// madam -> True
+// adamm -> True
+// junk -> False
+
+// Hint
+// The brute force approach would be to generate all the permutations of the string and check each one of them whether it is a palindrome. However, an optimized approach will not require this at all.
+
 function permuteAPalindrome (input) {
   // empty object to hold letters and letter counts
   const countLetters = {};
@@ -34,6 +46,7 @@ function permuteAPalindrome (input) {
   return false;
 };
 
+// TEST CASES
 console.log(permuteAPalindrome("a")); // true
 console.log(permuteAPalindrome("baa")); // true
 console.log(permuteAPalindrome("baad")); // false
