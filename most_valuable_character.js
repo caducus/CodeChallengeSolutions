001bc0d46be1f9980c53030e24d5c3d924226529
Becka Catanzaro <becka@Beckas-MacBook-Pro.local>
2019-12-21 16:39:42 -0500

    updated most_valuable_character.js with comments

--- most_valuable_character.js
+++ most_valuable_character.js
@@ -1,3 +1,13 @@
+// https://www.codewars.com/kata/5dd5128f16eced000e4c42ba
+
+// In this Kata, you will be given a string and your task is to return the most valuable character. The value of a character is the difference between the index of its last occurrence and the index of its first occurrence. Return the character that has the highest value. If there is a tie, return the lexicographically lowest character.
+
+// All inputs will be lower case.
+
+// For example:
+// solve('ab') = 'a'. Last occurrence is equal to first occurrence of each character. Return lexicographically lowest.
+// solve("axyzxyz") = 'x'
+
 function solve(st) {
   // create an empty object to hold the letter:value pairs
   let alphabetObject = {};
@@ -39,6 +49,7 @@ function solve(st) {
   return maxLetter[0];
 };
 
+// TEST CASES
 solve("a"); // a
 solve("aa"); // a
 solve("bcd"); // b
